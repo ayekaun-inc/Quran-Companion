3be5e974a58435ba9a4de8262db7accfce71b629
Sofstica-Muhammad-Aun <muhammad.aun@sofstica.com>
2024-08-09 07:08:34 +0500

    ongoing

--- app/lib/global/widgets/bottom_sheets/note_bottom_sheet/note_bottom_sheet.dart
+++ app/lib/global/widgets/bottom_sheets/note_bottom_sheet/note_bottom_sheet.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:quran_companion/global/services/size_helper_service.dart';
+import 'package:quran_companion/global/themes/colors.dart';
+import 'package:quran_companion/global/themes/fonts.dart';
+import 'package:quran_companion/global/widgets/custom_text.dart';
+import 'package:quran_companion/global/widgets/horizontal_padding.dart';
+import 'package:quran_companion/global/widgets/vertical_gap.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class NoteBottomSheet extends StatelessWidget {
@@ -14,9 +19,42 @@ class NoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50.h,
+      height: 370.h,
       width: double.maxFinite,
-      color: Colors.amberAccent,
+      decoration: ShapeDecoration(
+        color: white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(15.r),
+            topRight: Radius.circular(15.r),
+          ),
+        ),
+      ),
+      child: HorizontalPadding(
+        padding: 24,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const VerticalGap(16),
+            Center(
+              child: Container(
+                width: 48.w,
+                height: 4.h,
+                decoration: BoxDecoration(
+                  color: green.withOpacity(0.369),
+                  borderRadius: BorderRadius.circular(100.r),
+                ),
+              ),
+            ),
+            const VerticalGap(32),
+            CustomText(
+              'Make a note',
+              size: 14,
+              font: poppinsMedium,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
